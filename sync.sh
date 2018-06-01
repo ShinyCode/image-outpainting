d15926a06d73505291904058454b72c999ce0534
ShinyCode <ShinyCode@users.noreply.github.com>
2018-05-31 19:35:33 -0700

    Modify sync.sh to use rsync, to only copy new files.

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-scp -i "cs_230_key.pem" ubuntu@34.237.120.117:~/image-outpainting/output/*.png $1
+rsync -av --ignore-existing -e "ssh -i cs_230_key.pem" ubuntu@34.237.120.117:~/image-outpainting/output/*.png $1
