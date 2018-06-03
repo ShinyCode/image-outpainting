ba17fe2451855e3011156177cbd23fa2df6962f4
ShinyCode <ShinyCode@users.noreply.github.com>
2018-06-02 23:29:38 -0700

    Add figs to repo.

---
+++ figs.py
@@ -0,0 +1,18 @@
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import util
+
+IMAGE_SZ = 128
+
+def resize(in_PATH, out_PATH):
+    img = Image.open(in_PATH).convert('RGB')
+    img_scale = img.resize((IMAGE_SZ, IMAGE_SZ), Image.ANTIALIAS)
+    img_scale.save(out_PATH, format='PNG')
+
+def mask(in_PATH, out_PATH):
+    img = np.array(Image.open(in_PATH).convert('RGB'))
+    pix_avg = np.mean(img)
+    img[:, :int(2 * IMAGE_SZ / 8), :] = img[:, int(-2 * IMAGE_SZ / 8):, :] = pix_avg
+    img = Image.fromarray(img.astype(np.uint8), 'RGB')
+    img.save(out_PATH, format='PNG')
